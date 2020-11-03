117163f2790ea961d69a41e02f4f1d4bc4687bad
BernardCooley <bernardcooley@gmail.com>
2020-11-03 20:00:54 +0000

    grid click changed to filter

--- components/Grid.js
+++ components/Grid.js
@@ -6,6 +6,7 @@ import { usePlayerContext } from '../contexts/PlayerContext';
 import ClickGridCoordinates from '../data/ClickGridCoordinates';
 
 const Grid = () => {
+    const range = 10;
     const playerContext = usePlayerContext();
     const playerImageSize = useSelector(state => state.playerImageSize);
 
@@ -13,20 +14,30 @@ const Grid = () => {
         clickedOnBar(Math.round(e.nativeEvent.locationX), Math.round(e.nativeEvent.locationY));
     }
 
-    const range = 10;
-
     const clickedOnBar = (x, y) => {
-        ClickGridCoordinates.forEach((coord, index) => {
-            if(coord.x > x - range && coord.x < x + range && coord.y > y - range && coord.y < y + range) {
-                playerContext.seekTo(playerContext.currentTrack.duration * index / 100);
-                return;
-            }
-        });
+        const validClickCoordinates = ClickGridCoordinates.filter(coord => coord.x > x - range && coord.x < x + range && coord.y > y - range && coord.y < y + range);
+
+        const clickedIndex = validClickCoordinates.length === 1 ? ClickGridCoordinates.indexOf(validClickCoordinates[0]) : ClickGridCoordinates.indexOf(getClosest(validClickCoordinates));
+
+        playerContext.seekTo(playerContext.currentTrack.duration * clickedIndex / 100);
+    }
+
+    const distance = point => {
+        return Math.pow(point.x, 2) + Math.pow(point.y, 2);
+    }
+
+    const getClosest = points => {
+        const closest = points.slice(1).reduce(function (min, p) {
+            if (distance(p) < min.d) min.point = p;
+            return min;
+        }, { point: points[0], d: distance(points[0]) }).point;
+
+        return closest;
     }
 
     return (
         <>
-            <View onTouchEnd={(e) => clickProgressBar(e)} style={{ width: playerImageSize, height: playerImageSize }}></View>
+            <View onTouchStart={(e) => clickProgressBar(e)} style={{ width: playerImageSize, height: playerImageSize }}></View>
         </>
     )
 }
