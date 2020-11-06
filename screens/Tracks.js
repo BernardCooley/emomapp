b2e367e56e83bdbf3942f16e1aeeafe37c1dddc1
Bernard Cooley <BCooley@macmillan.org.uk>
2020-11-06 10:24:19 +0000

    tracks search label

--- screens/Tracks.js
+++ screens/Tracks.js
@@ -1,10 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { View, StyleSheet, SafeAreaView, BackHandler, Alert, ScrollView, RefreshControl, Keyboard } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { useFocusEffect } from "@react-navigation/native";
 import TracksList from '../components/TracksList';
 import PropTypes from 'prop-types';
-import { Searchbar, ActivityIndicator, Title, IconButton } from 'react-native-paper';
+import { Searchbar, ActivityIndicator, Title, Text } from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
 
 import useFirebaseCall from '../hooks/useFirebaseCall';
@@ -17,9 +17,15 @@ const TracksScreen = ({ navigation }) => {
     const activityIndicator = useSelector(state => state.activityIndicator);
     const allTracks = useSelector(state => state.tracks);
     const [getTracks, error, getNextTracks, getTrackImages] = useFirebaseCall('tracks', 'id', 20);
-    const [refreshing, setRefreshing] = React.useState(false);
-    const [searchQuery, setSearchQuery] = React.useState('');
+    const [refreshing, setRefreshing] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [showingSearchResults, setShowingSearchResults] = useState(false);
 
+    useEffect(() => {
+        if(searchQuery.length === 0) {
+            refresh();
+        }
+    }, [searchQuery]);
 
     const wait = (timeout) => {
         return new Promise(resolve => {
@@ -31,6 +37,7 @@ const TracksScreen = ({ navigation }) => {
         setRefreshing(true);
         getTracks();
         wait(2000).then(() => setRefreshing(false));
+        setShowingSearchResults(false);
     };
 
     useFocusEffect(
@@ -72,10 +79,9 @@ const TracksScreen = ({ navigation }) => {
                 const allTracks = response.docs.map(doc => doc.data());
                 const filteredTracks = allTracks.filter(track => contains(track.artist, searchQuery) || contains(track.title, searchQuery));
 
+                setShowingSearchResults(true);
                 getTrackImages(filteredTracks);
             });
-
-            setSearchQuery('');
         } else {
             dispatch(setSnackbarMessage(`Search box is empty`));
         }
@@ -104,10 +110,12 @@ const TracksScreen = ({ navigation }) => {
                                 flexGrow: 1,
                                 justifyContent: 'space-between'
                             }}>
+                            {showingSearchResults &&
+                                <Text style={styles.resultsLabel}>{allTracks.length} results found for: "{searchQuery}"</Text>
+                            }
                             {allTracks.length > 0 ?
                                 <TracksList tracks={allTracks} navigation={navigation} /> :
                                 <>
-                                    <IconButton style={{ ...styles.backIcon }} onPress={() => navigation.push('ExploreTabs', { screen: 'Tracks' })} animated icon="keyboard-backspace" size={30} />
                                     <View style={styles.noTracksLabel}>
                                         <Title >No tracks found</Title>
                                     </View>
@@ -153,6 +161,10 @@ const styles = StyleSheet.create({
         position: 'absolute',
         left: 0,
         top: 0
+    },
+    resultsLabel: {
+        fontWeight: 'bold',
+        padding: 10
     }
 });
 
